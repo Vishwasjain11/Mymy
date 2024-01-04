aaa23a6ede95004a104f9aa3621944f5c59b18b1
Pankaj Meharchandani <pmeharchandani@yahoo.com>
2024-01-04 22:43:40 +0530

    pankaj commit 2

--- app/src/main/java/com/example/mymy/MainActivity.java
+++ app/src/main/java/com/example/mymy/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hfcgui", Toast.LENGTH_SHORT).show();
     }
 }
